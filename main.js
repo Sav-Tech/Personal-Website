8eb1e643b0674b7f8e00273952f70026eb384955
Sav-Tech <79387863+Sav-Tech@users.noreply.github.com>
2021-04-04 10:19:37 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function one() {
+    window.alert("This is a picture of me and my mom in Hong Kong (Wan Chai) during Christmas")
+}
+
+function two() {
+    window.alert("This is a picture of me and my family during and Indian festivial called Diwali. We all gathered up and ate food with out friends and family.")
+}
+
+function three() {
+    window.alert("This is a picture of me standing in Hong Kong (Pacific Club) on a cruise ship! It was a 4 day cruise ship!")
+}
+
+function four() {
+    window.alert("This is a picture of me in Japan (Tokyo) in a bamboo forest")
+}
